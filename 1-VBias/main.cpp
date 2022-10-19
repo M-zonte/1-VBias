3dc4593011dee67c21b4da84b6b887c11fa96d4c
M_zonte <matteocapitani01@gmail.com>
2022-10-19 16:59:38 +0200

    Initial Commit

---
+++ 1-VBias/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1-VBias
+//
+//  Created by Matteo Capitani on 19/10/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
